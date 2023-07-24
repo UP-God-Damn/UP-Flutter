6b44f820eb57cc47fc6007094c096a40789f2216
ljyo2o9 <yggosxoljy@gmail.com>
2023-07-24 20:50:38 +0900

    에러/해결 드롭다운 버튼 제작

---
+++ lib/widget/errorDropdown.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ErrorDropdown extends StatefulWidget {
+  const ErrorDropdown({super.key});
+
+  @override
+  State<ErrorDropdown> createState() => _ErrorDropdownState();
+}
+
+class _ErrorDropdownState extends State<ErrorDropdown> {
+  final List<String> major = ['오류', '해결'];
+  String? selectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButtonHideUnderline(
+      child: DropdownButton2<String>(
+        isExpanded: true,
+        iconStyleData: IconStyleData(iconSize: 15.w),
+        hint: Text(
+          '오류/해결',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 10,
+            color: Theme.of(context).hintColor,
+          ),
+        ),
+        items: major
+            .map((String item) => DropdownMenuItem<String>(
+                  value: item,
+                  child: Text(
+                    item,
+                    style: const TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ))
+            .toList(),
+        value: selectedValue,
+        onChanged: (String? value) {
+          setState(() {
+            selectedValue = value;
+            print(value);
+          });
+        },
+        buttonStyleData: const ButtonStyleData(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          height: 40,
+          width: 140,
+        ),
+        menuItemStyleData: const MenuItemStyleData(
+          height: 40,
+        ),
+      ),
+    );
+  }
+}
