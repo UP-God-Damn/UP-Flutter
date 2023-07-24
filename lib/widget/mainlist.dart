b6c1973457da144916ffcc68be08b6bb9c4764f3
ljyo2o9 <yggosxoljy@gmail.com>
2023-07-24 23:03:16 +0900

    미리보기용 리스트

---
+++ lib/widget/mainlist.dart
@@ -0,0 +1,25 @@
+//제목, 작성자이름, 전공, 오류해결, 날짜
+
+final List<Map<String, dynamic>> mainList = [
+  {
+    'title': '오류 좀 고쳐주세요',
+    'name': '작성자이름',
+    'major': '플러터',
+    'tag': false,
+    'day': '2023/ 7/ 19',
+  },
+  {
+    'title': '내가 해결법을 알아냈다',
+    'name': '이재영',
+    'major': '프론트엔드',
+    'tag': true,
+    'day': '2023/ 7/ 19',
+  },
+  {
+    'title': '살려주세요',
+    'name': '이재영',
+    'major': '백엔드',
+    'tag': false,
+    'day': '2023/ 7/ 19',
+  },
+];
