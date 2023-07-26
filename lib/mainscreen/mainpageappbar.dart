0352b86750c0187a2e2b97d46f51cf879012d712
ljyo2o9 <yggosxoljy@gmail.com>
2023-07-26 10:22:48 +0900

    Appbar에서 드로어 뷰 수정

--- lib/mainscreen/mainpageappbar.dart
+++ lib/mainscreen/mainpageappbar.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-AppBar mainPageAppbar() {
+AppBar mainPageAppbar(context) {
   return AppBar(
     backgroundColor: const Color(0xFFF3F3F3),
-    leading: const Icon(Icons.menu, color: Colors.black),
+    leading: Builder(
+      builder: (context) {
+        return IconButton(
+          onPressed: () {
+            Scaffold.of(context).openDrawer();
+          },
+          icon: const Icon(Icons.menu, color: Colors.black),
+        );
+      },
+    ),
     title: Image.asset('assets/img/slogo.png',
         color: Colors.black, width: 43.w, height: 22.96.h),
     actions: [
