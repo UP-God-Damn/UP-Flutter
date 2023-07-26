dbb08603c7c1bd2552f68810ba25d7f51a34b878
ljyo2o9 <yggosxoljy@gmail.com>
2023-07-26 09:41:19 +0900

    글 쓰기 페이지 완성

--- lib/create/create.dart
+++ lib/create/create.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:up/create/createappbar.dart';
+import 'package:up/create/createbody.dart';
 
 class Create extends StatefulWidget {
   const Create({super.key});
@@ -11,6 +12,10 @@ class Create extends StatefulWidget {
 class _CreateState extends State<Create> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xFFF3F3F3),
+      appBar: createAppbar(context),
+      body: createBody(context),
+    );
   }
 }
