94be558a01bdcfcf24e384769c353dfa4ecdfd08
ljyo2o9 <yggosxoljy@gmail.com>
2023-07-26 10:56:21 +0900

    Details로 데이터 넘기기

--- lib/mainscreen/mainpagebody.dart
+++ lib/mainscreen/mainpagebody.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:up/viewdetails/detailssaffold.dart';
 
 import 'package:up/widget/majorDropdown.dart';
 import 'package:up/widget/errorDropdown.dart';
@@ -71,104 +72,121 @@ Column mainPageBody() {
           child: ListView.builder(
             itemCount: mainList.length,
             itemBuilder: (context, index) {
+              String title = mainList[index]['title'];
+
+              // onPressed: () {
+              // Navigator.of(context).push(
+              // MaterialPageRoute(
+              // builder: (BuildContext context) => TwoScreen(),
+              // settings: RouteSettings(arguments: '전송한 데이터'),
+              // ),
+              // );
               return Padding(
                 padding: EdgeInsets.only(right: 20.w, left: 20.w, bottom: 10.h),
-                child: Container(
-                  width: 390.w,
-                  height: 95.h,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(top: 20.h),
-                        child: Row(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(left: 16.w),
-                              child: Container(
-                                width: 44.w,
-                                height: 15.h,
-                                decoration: BoxDecoration(
-                                  color: mainList[index]['tag'] == true
-                                      ? const Color(0xFF7DB45A)
-                                      : const Color(0xFFDA6156),
-                                  borderRadius: const BorderRadius.all(
-                                      Radius.circular(10)),
-                                ),
-                                child: Center(
-                                  child: Text(
-                                    mainList[index]['tag'] == true
-                                        ? '해결'
-                                        : '질문',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: 9.sp,
-                                      fontFamily: 'NotoSansKR',
-                                      color: Colors.black,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const Details(),
+                      settings: RouteSettings(arguments: title),
+                    ));
+                  },
+                  child: Container(
+                    width: 390.w,
+                    height: 95.h,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 20.h),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(left: 16.w),
+                                child: Container(
+                                  width: 44.w,
+                                  height: 15.h,
+                                  decoration: BoxDecoration(
+                                    color: mainList[index]['tag'] == true
+                                        ? const Color(0xFF7DB45A)
+                                        : const Color(0xFFDA6156),
+                                    borderRadius: const BorderRadius.all(
+                                        Radius.circular(10)),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      mainList[index]['tag'] == true
+                                          ? '해결'
+                                          : '질문',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        fontSize: 9.sp,
+                                        fontFamily: 'NotoSansKR',
+                                        color: Colors.black,
+                                      ),
                                     ),
                                   ),
                                 ),
                               ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(left: 9.w),
-                              child: Text(
-                                mainList[index]['title'],
-                                style: TextStyle(
-                                  fontSize: 17.sp,
-                                  fontWeight: FontWeight.w500,
-                                  fontStyle: FontStyle.normal,
+                              Padding(
+                                padding: EdgeInsets.only(left: 9.w),
+                                child: Text(
+                                  mainList[index]['title'],
+                                  style: TextStyle(
+                                    fontSize: 17.sp,
+                                    fontWeight: FontWeight.w500,
+                                    fontStyle: FontStyle.normal,
+                                  ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(left: 16.w, top: 3.h),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              mainList[index]['name'],
-                              style: TextStyle(
-                                fontSize: 9.sp,
-                                fontFamily: 'NotoSansKR',
-                                fontWeight: FontWeight.w400,
+                        Padding(
+                          padding: EdgeInsets.only(left: 16.w, top: 3.h),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                mainList[index]['name'],
+                                style: TextStyle(
+                                  fontSize: 9.sp,
+                                  fontFamily: 'NotoSansKR',
+                                  fontWeight: FontWeight.w400,
+                                ),
                               ),
-                            ),
-                            Text(
-                              mainList[index]['major'],
-                              style: TextStyle(
-                                fontSize: 9.sp,
-                                fontFamily: 'NotoSansKR',
-                                fontWeight: FontWeight.w400,
-                              ),
-                            )
-                          ],
+                              Text(
+                                mainList[index]['major'],
+                                style: TextStyle(
+                                  fontSize: 9.sp,
+                                  fontFamily: 'NotoSansKR',
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(right: 14.w),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            Text(
-                              mainList[index]['day'],
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 8.sp,
-                                fontWeight: FontWeight.w100,
+                        Padding(
+                          padding: EdgeInsets.only(right: 14.w),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                mainList[index]['day'],
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 8.sp,
+                                  fontWeight: FontWeight.w100,
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               );
