8882ed113e2a5a1c282d0bf8e8f055249f52abb3
ljyo2o9 <yggosxoljy@gmail.com>
2023-07-26 10:55:38 +0900

    Details Appbar뷰 완성

---
+++ lib/viewdetails/detailsappbar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+AppBar detailsAppbar(title, context) {
+  return AppBar(
+    backgroundColor: const Color(0xFFF3F3F3),
+    centerTitle: false,
+    leading: IconButton(
+      icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    ),
+    elevation: 3,
+    title: Text(
+      title,
+      style: TextStyle(
+        fontSize: 20.sp,
+        fontFamily: 'NotoSansKR',
+        fontWeight: FontWeight.w200,
+        color: Colors.black,
+      ),
+    ),
+  );
+}
