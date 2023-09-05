2f3936cf40bfb610a649cde5a15fa0db4b5dc538
ljyo2o9 <yggosxoljy@gmail.com>
2023-09-05 19:24:46 +0900

    Provider 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'Sign in and Sign up/choose.dart';
+import 'provider/error_provider.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(const MyApp());
 }
 
@@ -17,14 +20,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     FlutterNativeSplash.remove();
 
-    return ScreenUtilInit(
-      designSize: const Size(430, 932),
-      builder: (context, child) {
-        return const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: SafeArea(child: Choose()),
-        );
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ErrorController()),
+        ChangeNotifierProvider(create: (context) => MajorController()),
+      ],
+      child: ScreenUtilInit(
+        designSize: const Size(430, 932),
+        builder: (context, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: const SafeArea(child: Choose()),
+          );
+        },
+      ),
     );
   }
 }
