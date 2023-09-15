776fbaba4369d27bd8895ac31c89d607acfadd4d
ljyo2o9 <yggosxoljy@gmail.com>
2023-09-15 11:28:28 +0900

    댓글 Post 기능 완성

--- lib/reply/reply.dart
+++ lib/reply/reply.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'dart:convert';
 import 'dart:io';
 
@@ -10,6 +12,27 @@ import 'package:up/model/userDetails.dart';
 import 'package:up/url.dart';
 
 enum SampleItem { modify, del }
+
+Future postComment(id, comment) async {
+  final url = '$baseUrl/comment';
+  const storage = FlutterSecureStorage();
+  final token = await storage.read(key: 'accessToken');
+  final response = await http.post(
+    Uri.parse(url),
+    headers: {
+      'Content-Type': 'application/json',
+      HttpHeaders.authorizationHeader: 'Bearer $token',
+    },
+    body: jsonEncode({
+      "id": id,
+      "content": comment,
+    }),
+  );
+
+  if (response.statusCode != 201) {
+    throw Exception(response.body);
+  }
+}
 
 class CommentDetails extends StatefulWidget {
   final String id;
@@ -50,8 +73,10 @@ class _CommentDetailsState extends State<CommentDetails> {
 
   @override
   Widget build(BuildContext context) {
-    final commentController = TextEditingController();
+    final commentTextController = TextEditingController();
     SampleItem? selectedMenu;
+
+    final id = widget.id;
 
     return Scaffold(
         backgroundColor: const Color(0xFFF3F3F3),
@@ -236,18 +261,86 @@ class _CommentDetailsState extends State<CommentDetails> {
                       ),
                     ),
                   ),
-                  SizedBox(
+                  Container(
                     width: MediaQuery.of(context).size.width,
-                    height: 60.h,
+                    height: 120.h,
+                    decoration: BoxDecoration(
+                        border: Border(
+                            top: BorderSide(
+                      width: 1.h,
+                      color: Colors.black,
+                    ))),
                     child: Center(
                       child: Padding(
                         padding: EdgeInsets.only(left: 20.w),
                         child: TextField(
-                          controller: commentController,
+                          controller: commentTextController,
                           maxLines: 1000,
-                          decoration: const InputDecoration(
-                            suffixIcon: Icon(
-                              Icons.send,
+                          decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              padding: EdgeInsets.only(
+                                  bottom: double.infinity, top: 10.h),
+                              onPressed: () async {
+                                if (commentTextController.text.length < 5) {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Text('댓글이 생성되지 않았습니다.'),
+                                        content: const Text('글자수 제한(5글자 이하)'),
+                                        actions: [
+                                          MaterialButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Text('확인'),
+                                          )
+                                        ],
+                                      );
+                                    },
+                                  );
+                                } else if (commentTextController.text.length >
+                                    5000) {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Text('댓글이 생성되지 않았습니다.'),
+                                        content:
+                                            const Text('글자수 제한(5000글자 이상)'),
+                                        actions: [
+                                          MaterialButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Text('확인'),
+                                          )
+                                        ],
+                                      );
+                                    },
+                                  );
+                                } else {
+                                  postComment(id, commentTextController.text);
+                                  await showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        title: const Text('댓글이 생성되었습니다'),
+                                        actions: [
+                                          MaterialButton(
+                                            onPressed: () {
+                                              Navigator.pop(context);
+                                            },
+                                            child: const Text('확인'),
+                                          )
+                                        ],
+                                      );
+                                    },
+                                  );
+                                  Navigator.pop(context);
+                                }
+                              },
+                              icon: const Icon(Icons.send),
                               color: Colors.black,
                             ),
                             hintText: '댓글을 작성해 주세요',
