4d4aba095cafd45bef606268446c603b29413296
ljyo2o9 <yggosxoljy@gmail.com>
2023-09-20 08:01:19 +0900

    로그아웃 구현

--- lib/mainscreen/mainpageappbar.dart
+++ lib/mainscreen/mainpageappbar.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:up/Sign%20in%20and%20Sign%20up/choose.dart';
 
 AppBar mainPageAppbar(context) {
   return AppBar(
     backgroundColor: const Color(0xFFF3F3F3),
+    centerTitle: true,
+    elevation: 4,
+    title: Image.asset(
+      'assets/img/slogo.png',
+      color: Colors.black,
+      width: 43.w,
+      height: 22.96.h,
+    ),
     leading: Builder(
       builder: (context) {
         return IconButton(
@@ -15,13 +24,40 @@ AppBar mainPageAppbar(context) {
         );
       },
     ),
-    title: Image.asset(
-      'assets/img/slogo.png',
-      color: Colors.black,
-      width: 43.w,
-      height: 22.96.h,
-    ),
-    centerTitle: true,
-    elevation: 4,
+    actions: [
+      IconButton(
+        onPressed: () async {
+          await showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('로그아웃을 하시겠습니까?'),
+                actions: [
+                  MaterialButton(
+                      onPressed: () {
+                        Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                              builder: (context) => const Choose(),
+                            ),
+                                (route) => false);
+                      },
+                      child: const Text('예')),
+                  MaterialButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('아니요'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        icon: const Icon(
+          Icons.logout,
+          color: Colors.black,
+        ),
+      )
+    ],
   );
 }
